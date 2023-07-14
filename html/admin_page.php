7ce5ad28d698fb65a5767b20fb580a053de6a404
Isaiah012 <isaiahagapito8@gmail.com>
2023-07-15 00:23:58 +0800

    admin page

---
+++ html/admin_page.php
@@ -0,0 +1,21 @@
+<!DOCTPYE html>
+<html>
+ <head>
+    <meta charset = "UTF-8">
+    <meta charset name = "viewport" content = "width=device-width, initial-scale-1.0">
+    <title>Lakbay Tips Admin</title>
+
+    <!-- css file link -->
+    <link rel="stylesheet" type="text/css" href="../css/adminstyle.css">
+
+ </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h3>hi, <span>admin</span></h3>
+                <h1>welcome <span></span></h1>
+                <p>admin user only</p>
+            </div>
+        </div>
+    </body>
+</html>
